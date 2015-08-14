66b53949518c939d14e22a6f27e854ed8dc172b2
t-harps <t.w.harps@gmail.com>
2015-08-14 16:32:30 +0100

    Add Post and Comments models with associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	has_many :posts
+	has_many :comments
+	
+	validates :username, presence: true, length: {maximum: 15}, uniqueness: {case_sesitive: false}
+	validates :password, presence: true, length: {minimum: 6}
 end
